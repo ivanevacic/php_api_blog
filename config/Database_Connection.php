521cf15462e07e125e93086965325282a5b8f897
ivanevacic <ivanevacicweb@gmail.com>
2018-06-17 09:47:58 +0200

    Added  database connection

---
+++ config/Database_Connection.php
@@ -0,0 +1,23 @@
+<?php
+
+class Database {
+    //  DB Parameters
+    private $host = 'localhost';
+    private $db_name = 'php_api_blog';
+    private $username = 'root';
+    private $password = '';
+    private $connection;
+
+    //DB Connect
+    public function connect() {
+        $this->connection = null;
+        try {
+            $this->connection = new PDO('mysql:host=' . $this->host . ';dbname= ' . $this->db_name, $this->username, $this->password);
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }   catch(PDOException $e) {
+            echo 'Connection Error ' . $e->getMessage();
+        }
+
+        return $this->connection;
+    }
+}
