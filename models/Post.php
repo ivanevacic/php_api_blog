491c1cca74c534cef0accbf126e445520bd3a5de
ivanevacic <ivanevacicweb@gmail.com>
2018-06-17 09:58:02 +0200

    Added Post model

---
+++ models/Post.php
@@ -0,0 +1,47 @@
+<?php
+
+    class Post {
+        private $connection;
+        private $table = 'posts';
+
+        public $id;
+        public $category_id;
+        public $category_name;
+        public $title;
+        public $body;
+        public $author;
+        public $created_at;
+
+        //  Constructor
+        public function __construct($db) {
+            $this->connection = $db;
+        }
+
+        //  Get post
+        public function read() {
+            //  Create query
+            $query = 'SELECT 
+                c.name as category_name,
+                p.id,
+                p.category_id,
+                p.title,
+                p.body,
+                p.author,
+                p.created_at
+            FROM
+                ' . $this->table . ' p
+            LEFT JOIN
+                categories c ON p.category_id = c.id
+            ORDER BY 
+                p.created_at DESC';
+
+        
+        //  Prepare statement
+            $stmt = $this->connection->prepare($query);
+            //  Execute statement
+            $stmt->execute();
+            //  Return statement
+            return $stmt;
+
+        }
+}
